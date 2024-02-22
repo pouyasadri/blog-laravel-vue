5101d12d150bd17f5610aa0287426b4f792ea86d
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-22 17:23:25 +0100

    feat: add article migration, seeder,factory, controller, model

---
+++ api/blog_api/database/factories/ArticleFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(6, true),
+            'content' => $this->faker->paragraph(6, true),
+            'category' => $this->faker->randomElement(['news', 'updates', 'reports', 'stories']),
+            'image' => 'default.jpg',
+        ];
+    }
+}
